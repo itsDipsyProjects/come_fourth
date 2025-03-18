6088b36065eb88e9e0b0857fad02f2996d62d3c1
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2025-03-18 14:13:47 +0100

    Added lava damage to the lava pit, if collision the player will respond (instant)

--- player/player.gd
+++ player/player.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 300
-@export var gravity = 1000  # Earth's gravity in pixels/sec²
-@export var jump_force = -400  # Negative because jumping moves the player upwards
+@export var gravity = 1000 
+@export var jump_force = -400  
 
 func get_input():
 	var input_direction = Input.get_axis("left", "right")
 	velocity.x = input_direction * speed
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_force  # Apply jump force if on the floor
+		velocity.y = jump_force  
 
 func _physics_process(delta):
 	# Apply gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta  # Integrate gravity over time
+		velocity.y += gravity * delta  
 	
 	get_input()
 	move_and_slide()
